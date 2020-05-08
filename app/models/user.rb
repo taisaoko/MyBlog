abf2d641b3c89f5207a195c005d0950ecd4caa96
taisaoko <ttd3calvin@gmail.com>
2020-05-08 10:57:55 -0400

    update gemfile and routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
 
     has_many :comments
     has_many :commented_posts, through: :comments, source: :post
+    has_secure_password #authenticate, validate pw &/or pw confirmation
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
+
+  get '/signup' => 'users#new'
+  post'/signup' => 'users#create'
+
+  # resources :categories
   resources :comments
   resources :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
